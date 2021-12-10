4802ad5f76b5646060613fcc81e5466bb6e07016
James Neate <jamesmneate@gmail.com>
2021-12-10 00:45:32 +0000

    Added 2021 - Day One

---
+++ 2021/TypeScript/Day 1/01.ts
@@ -0,0 +1,78 @@
+import events from 'events';
+import fs from 'fs';
+import { cwd } from 'process';
+import * as readline from 'readline';
+
+// const fileName = `${__dirname}\\test.txt`;
+const fileName = `${__dirname}\\inputs.txt`;
+
+const dataContainer: number[] = [];
+
+let result: number = 0;
+let resultTwo: number = 0;
+
+async function loadFile() {
+  try {
+    const readStream = readline.createInterface({
+      input: fs.createReadStream(fileName),
+      crlfDelay: Infinity
+    });
+
+    readStream.on('line', (line) => {
+      dataContainer.push(Number(line.trim()))
+    });
+
+    await events.once(readStream, 'close');
+
+    console.log('File Read');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+async function processPartOne() {
+
+  for (let i = 0; i < dataContainer.length; i++) {
+    
+    if (i === 0) {
+      continue;
+    }
+
+    if (dataContainer[i] > dataContainer[i-1]) {
+      result = result + 1;
+    }
+
+
+  }
+
+};
+
+async function processPartTwo() {
+
+  for (let i = 0; i < dataContainer.length; i++) {
+    
+    if (i < 3) {
+      continue;
+    }
+
+    const groupOneSum = dataContainer[i - 3] + dataContainer[i - 2] + dataContainer[i - 1];
+    const groupTwoSum = dataContainer[i - 2] + dataContainer[i - 1] + dataContainer[i];
+
+    if (groupTwoSum > groupOneSum) {
+      resultTwo = resultTwo + 1;
+    }
+
+  }
+
+};
+
+async function start() {
+  console.log('Reading File');
+  await loadFile();
+  await processPartOne();
+  console.log(result);
+  await processPartTwo();
+  console.log(resultTwo);
+};
+
+start();
--- README.md
+++ README.md
@@ -10,9 +10,13 @@ Any feedback is welcome, feel free to raise issues or contribute better solution
 
 P.S. - For the eagle eyed amongst you, yes I started at 2020. The plan is to go through years 2015-2019 after I have completed 2020! Or re-do 2020 in different language(s)....
 
+## Updates
+
++ Added 2021 and started using TypeScript :rocket: (Be nice to get beyond Day 6 this time before :beer: kicks in)
+
 ## Day Specific Notes
 
-### Day 5
+### 2020 - Day 5
 
 Day 5 was an interesting one for me, my first thought was this is screaming a Binary Search Tree but I couldn't quickly get my head around the data structure, whether this is because it was around midnight and I'd had a couple of beers or not I'm not sure, instead I went for a slightly messy implementation but, in theory, it should be easy enough to extend if the number of columns or rows increased on the plane! But still, a few too many Arrays even for my liking.
 
